422337d4c316b94d67656bafa990028190fdcf6e
Jerry Yuan <yhyuan@gmail.com>
2015-03-26 20:38:27 -0400

    translation

--- model.js
+++ model.js
@@ -3,6 +3,25 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 // Parties
+getUserLanguage = function () {
+  // Put here the logic for determining the user language
+  return "zh-CN";
+};
+
+if (Meteor.isClient) {
+  Meteor.startup(function () {
+    Session.set("showLoadingIndicator", true);
+
+    TAPi18n.setLanguage(getUserLanguage())
+      .done(function () {
+        Session.set("showLoadingIndicator", false);
+      })
+      .fail(function (error_message) {
+        // Handle the situation
+        console.log(error_message);
+      });
+  });
+}
 
 /*
   Each party is represented by a document in the Parties collection:
