b9f8a1b6c3b3950e3522397f4b26d5ddda396309
Jerry Yuan <yhyuan@gmail.com>
2015-04-16 22:47:27 -0400

    Add acitivity

--- client/lib/bootstrap-datepicker.js
+++ client/lib/bootstrap-datepicker.js
@@ -371,8 +371,13 @@
 		dates:{
 			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
 			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+			//daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+			daysMin: ["日", "一", "二", "三", "四", "五", "六", "日"],
+			/*
 			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+			*/
+			months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
 			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 		},
 		isLeapYear: function (year) {
--- client/views/AddActivities.js
+++ client/views/AddActivities.js
@@ -1,100 +1,53 @@
 
+Template.AddActivities.rendered = function(){
+  $('#activityDate').datepicker({
+    format: 'yyyy-mm-dd'
+  });
+};
+
 Template.AddActivities.helpers({
   currentCropYear: function () {
     var cropsYears =  Session.get("currentViewedFieldCropsYears");
     var firstCropYear = cropsYears[Session.get("currentViewedFieldCropsYearsIndex")];
     return firstCropYear.name + ' -- ' + firstCropYear.year + ' (' + Session.get("currentViewedField").title + ')';
   },
+  currentDate: function () {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    } 
+
+    if(mm<10) {
+        mm='0'+mm
+    } 
+
+    return yyyy + '-' + mm + '-' + dd;
+   
+  },
   activities: function () {
     return Activities.find({});
-  },
-
-  isActivitiesZero: function () {
-    var cropsYears =  Session.get("currentViewedFieldCropsYears");
-    var firstCropYear = cropsYears[Session.get("currentViewedFieldCropsYearsIndex")];
-    //console.log(firstCropYear);
-    var year = firstCropYear.year;
-    var cropId = firstCropYear.cropId;
-    var activities = Session.get("currentViewedField").activities;
-    if (!activities.hasOwnProperty(year)) {
-      return true;
-    }
-    if (!activities[year].hasOwnProperty(cropId)) {
-      return true;
-    } 
-    return false;
-  },
-  activitiesList: function () {
-    var cropsYears =  Session.get("currentViewedFieldCropsYears");
-    var firstCropYear = cropsYears[Session.get("currentViewedFieldCropsYearsIndex")];
-    var year = firstCropYear.year;
-    var cropId = firstCropYear.cropId;
-    var activitiesCurrentField = Session.get("currentViewedField").activities;
-
-    var activities = Activities.find({}).fetch();
-    var activitiesDict = _.object(_.map(activities, function(activity) {return activity.Id;}), _.map(activities, function(activity) {return activity.name;}));  
-    return _.map(activitiesCurrentField[year][cropId], function(act) {
-      return {date: act.date, activity: activitiesDict[act.activity], performer: act.performer, activityId: act.activity, performerId: act.performer};
-    });
-  },
-  cropsYears: function () {
-    return Session.get("currentViewedFieldCropsYears");
-  }/*,
-  yearCropsInField: function () {
-    var crops = Crops.find({}).fetch();
-    var cropsDict = _.object(_.map(crops, function(crop) {return crop.Id;}), _.map(crops, function(crop) {return crop.name;}));
-    var results = _.map(_.keys(Session.get("currentViewedFieldCrops")), function(year) {
-      var cropsInField = _.map(Session.get("currentViewedFieldCrops")[year], function(cropId) {
-        return {Id: cropId, name: cropsDict[cropId], year: year};
-      });
-      return {year: year, cropsInField: cropsInField};
-    });
-    results = results.sort(function(a,b) { return b.year - a.year; });
-    console.log(results);
-    return results;
-  },
-  years: function () {
-    return _.map(_.range(new Date().getFullYear(), 2000, -1), function(year) {
-      return {name: year};
-    });
-  }*/
+  }
 });
 
+Template.AddActivities.events({
+    'click #saveActivityButton' : function(event, template){
+         //var el = template.loading;
+      console.log('OK OK');     
+      Session.set('addingActivities', false);    
+    }
+});
+/*
 if (!window.ZhiHuiFarmUI) {
     window.ZhiHuiFarmUI = {};
 }
-/*
-window.ZhiHuiFarmUI.saveCrops = function () {
-  var field = _.clone(Session.get("currentViewedField"));
-  field.crops = Session.get("currentViewedFieldCrops");
-  Session.set("currentViewedField", field);
-  updateParty({
-      _id: field._id,
-      title: field.title,
-      description: field.description,
-      boundary: field.boundary,
-      crops: field.crops
-  });      
-  //console.log(Session.get("currentViewedField"));
-  Session.set("isCropsBeingEdited", false);
+
+window.ZhiHuiFarmUI.saveActivity = function () {
+//  Session.set('addingActivities', true);
+  console.log('OK');
 };
+
 */
-window.ZhiHuiFarmUI.deleteActivity = function (year, id) {
-  var currentCrops = _.clone(Session.get("currentViewedFieldCrops"));
-  currentCrops[year] = _.filter(currentCrops[year], function(currentId) {
-    return currentId !== id;
-  });
-
-  if (currentCrops[year].length === 0) {
-    delete currentCrops[year];
-  }
-  Session.set("currentViewedFieldCrops", currentCrops)
-};
-
-window.ZhiHuiFarmUI.addActivity = function () {
-  Session.set('addingActivities', true);
-};
-
-window.ZhiHuiFarmUI.CropsYearsSelectListChanged = function (value) {
-  Session.set("currentViewedFieldCropsYearsIndex", parseInt(value));
-};
